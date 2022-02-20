6c2a692f94bd49487d9ba5f8467b71d1c5849151
Abdul-Mateen <matdboi@gmail.com>
2022-02-20 22:30:52 +0100

    fine-tuning + added resume live view

--- app.R
+++ app.R
@@ -2,17 +2,18 @@ library(shiny)
 library(markdown)
 
 themes <- c("blue", "yellow", "green", "red", "black")
+#themes_icon <- 
 class <- c("Programming", "Tools/Technologies", "Industry Knowledge",
            "Packages/Modules", "Other Skills", "Soft Skills")
 
 ui <- fluidPage(style = "padding-left: 40px;",
-  fluidRow(h3("CHOOSE YOUR PREFERRED THEME"),
+  fluidRow(h3("CHOOSE YOUR PREFERRED THEME", style = "color: #005090"),
            radioButtons("theme", "What colour scheme do you like?", themes)
            ),
   
   hr(),
   
-  fluidRow(h3("INPUT YOUR NAMES"),
+  fluidRow(h3("INPUT YOUR NAMES", style = "color: #005090"),
     column(4,
            textInput("f_n", "First name"),
            style = "padding-left:0px"
@@ -29,7 +30,7 @@ ui <- fluidPage(style = "padding-left: 40px;",
   
   hr(),
   
-  fluidRow(h3("INPUT YOUR CONTACT DETAILS"),
+  fluidRow(h3("INPUT YOUR CONTACT DETAILS", style = "color: #005090"),
     column(4,
            textInput("phone", "Phone number"),
            textInput("github", "Github profile link"),
@@ -49,44 +50,56 @@ ui <- fluidPage(style = "padding-left: 40px;",
   
   hr(),
   
-  fluidRow(h3("ENTER A SHORT DESCRIPTION OF YOU"),
+  fluidRow(h3("ENTER A SHORT DESCRIPTION OF YOU", style = "color: #005090"),
            textAreaInput("summary", "Summary", rows = 6, width = "700px"),
            ),
   
   hr(),
   
-  fluidRow(h3("ENTER YOUR INDUSTRY EXPERIENCE"),
+  fluidRow(h3("ENTER YOUR INDUSTRY EXPERIENCE", style = "color: #005090"),
            textInput("title", "Job title"),
-           textAreaInput("desc", "Job description", rows = 4, width = "400px")
-           ),
-  fluidRow(
-    column(2,
-           actionButton("add_new", "Submit & add New", width = "155px",
-                        style = "color:white; background-color:blue",
-                        icon = icon("plus")
+           textAreaInput("desc", "Job description", rows = 4, width = "400px"),
+           actionButton("add_new_1", "Submit & add New", width = "155px",
+                        style = "color:white; background-color:#005090",
+                        icon = icon("plus-circle")
                         ),
-           style = "padding-left:0px"
-           ),
-    column(10,
+           br(), br(),
            p('Click "Submit & add New" to add new experience (Re-enter after the text box refrehes)',
-             style = "color: red")
-           )
-  ),
+             style = "color: red"
+             )
+           ),
   
   hr(),
   
-  fluidRow(h3("ENTER YOUR SKILLS"),
+  fluidRow(h3("ENTER YOUR SKILLS", style = "color: #005090"),
            selectInput("class", "Class of skill", choices = class),
            selectInput("sub_class", "Sublass of skill", choices = NULL),
-           actionButton("add_new", "Submit & add New", width = "155px",
-                        style = "color:white; background-color:blue",
-                        icon = icon("plus")
+           actionButton("add_new_2", "Submit & add New", width = "155px",
+                        style = "color:white; background-color:#005090",
+                        icon = icon("plus-circle")
                         )
            ),
   
-  hr(),
+  br(), br(), #hr(style = "border-top: 1px solid #005090"),
   
-  includeMarkdown("Resume_2_2.md"),
+  fluidRow(
+    column(4, offset = 4,
+      actionButton("generate", "GENERATE RESUME", width = "300px", height = "120px",
+                   style = "color:white; background-color:#005090; font-weight:bold",
+                   icon = icon("file-text")
+      )
+    ),
+    style = "background-color: #e4e8eb"
+  ),
+  
+  fluidRow(
+    column(8, offset = 2,
+           includeMarkdown("Resume_2_2.md"),
+           #style = "height: 500px"
+           style = "border: 3px solid #005090"
+           ),
+    style = "background-color: #e4e8eb; padding-top: 50px; padding-bottom: 50px"
+  ),
   
   hr()
   
